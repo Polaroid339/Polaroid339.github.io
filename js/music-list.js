9055b95be9827dced73208e2f1066e0e3f90311f
Victor Oliveira <168303922+Polaroid339@users.noreply.github.com>
2024-09-26 20:09:26 -0300

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,22 @@
+// Array contendo todos os objetos de musica
+
+let allMusic = [
+    {
+        name: "Beat It", //Nome da musica
+        artista: "Michael Jackson", // Nome do artista
+        img: "music (1)", //Nome da imagem 
+        src: "music (1)" //nome do áudio
+    },
+    {
+        name: "Bad", //Nome da musica
+        artista: "Michael Jackson", // Nome do artista
+        img: "music (2)", //Nome da imagem 
+        src: "music (2)" //nome do áudio
+    },
+    {
+        name: "Smooth Criminal", //Nome da musica
+        artista: "Michael Jackson", // Nome do artista
+        img: "music (3)", // Nome da imagem 
+        src: "music (3)" // Nome do áudio
+    },
+]
---
+++ js/script.js
@@ -0,0 +1,27 @@
+const wrapper = document.querySelector(".wrapper"),
+musicImg = wrapper.querySelector(".img-area img"),
+musicName = wrapper.querySelector(".song-details .name"),
+musicArtist = wrapper.querySelector(".song-details .artist"),
+playPauseBtn = wrapper.querySelector(".play-pause"),
+prevBtn = wrapper.querySelector("#prev"),
+nextBtn = wrapper.querySelector("#next"),
+mainAudio = wrapper.querySelector("#main-audio"),
+progressArea = wrapper.querySelector(".progress-area"),
+musicList = wrapper.querySelector(".music-list"),
+progressBar = wrapper.querySelector(".progress-bar"),
+moreMusicBtn = wrapper.querySelector("#more-music"),
+closeMoreMusc = wrapper.querySelector("#close"),
+
+let musicIndex = Math.floor((Math.random() * allMusic.length) + 1);
+isMusicPaused = true;
+
+window.addEventListener("load", () => {
+    loadMusic(musicIndex);
+    playingSong();
+});
+
+function loadMusic(indexNumb){
+    musicName.innerText = allMusic[indexNumb - 1].name;
+    musicArtist.innerText = allMusic[indexNumb - 1].artist;
+    musicImg.src = `images/${allMusic[indexNumb - 1].src}.jpg`;
+}
