84691bdb795eb26175fbdbb6c642156e38fab5f2
Victor Oliveira <168303922+Polaroid339@users.noreply.github.com>
2024-11-08 15:31:10 -0300

    Update music-list.js

--- js/music-list.js
+++ js/music-list.js
@@ -1,22 +1,20 @@
-// Array contendo todos os objetos de musica
-
 let allMusic = [
     {
-        name: "Beat It", //Nome da musica
-        artista: "Michael Jackson", // Nome do artista
-        img: "music (1)", //Nome da imagem 
-        src: "music (1)" //nome do áudio
+      name: "Rock With You", // Nome da música
+      artist: "Michael Jackson", // Nome do artista
+      img: "music-1", // Nome do arquivo da imagem associada à música (sem extensão)
+      src: "music-1" // Nome do arquivo de áudio associado à música (sem extensão)
     },
     {
-        name: "Bad", //Nome da musica
-        artista: "Michael Jackson", // Nome do artista
-        img: "music (2)", //Nome da imagem 
-        src: "music (2)" //nome do áudio
+      name: "Say Say Say", // Nome da música
+      artist: "Michael Jackson", // Nome do artista
+      img: "music-2", // Nome do arquivo da imagem associada à música (sem extensão)
+      src: "music-2" // Nome do arquivo de áudio associado à música (sem extensão)
     },
     {
-        name: "Smooth Criminal", //Nome da musica
-        artista: "Michael Jackson", // Nome do artista
-        img: "music (3)", // Nome da imagem 
-        src: "music (3)" // Nome do áudio
+      name: "Smooth Criminal", // Nome da música
+      artist: "Michael Jackson", // Nome do artista
+      img: "music-3", // Nome do arquivo da imagem associada à música (sem extensão)
+      src: "music-3" // Nome do arquivo de áudio associado à música (sem extensão)
     },
-]
+];
